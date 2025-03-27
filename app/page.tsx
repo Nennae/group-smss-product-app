fb7e98b53fca18f17759eade88752fcf96644ab5
Nennae <sezenerdal@outlook.com>
2025-03-28 00:31:54 +0100

    refactor code for readability

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,11 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ p
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center">
         <CategoryBar />
         <h1 className="text-center text-black-300 mt-10">MIN Products Store</h1>
         <span>Totalt: {total} produkter</span>
         <CardList products={products} totalProducts={total} />
-
       </main>
     </div>
   );
